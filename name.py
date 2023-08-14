12629e4eda13fc05b97b51b6b29de07a76401fd8
UMSMASTA <paul.t.black34@gmail.com>
2023-08-14 21:37:05 +0000

    Lab 16

---
+++ name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+
+    user_input = input("Please enter your name: ")
+
+    print(f"Thank you, {user_input}!")
+
+    day_input = input(f"Please tell me the day of the week, {user_input}: ")
+
+    print("Thank you", user_input+"!", "Happy", day_input+"!")
+
+main()
