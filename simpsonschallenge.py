04bdb6d04f3cd6c18f0be54251402a904961d078
UMSMASTA <paul.t.black34@gmail.com>
2023-08-15 18:22:24 +0000

    updated Lab 27

--- simpsonschallenge.py
+++ simpsonschallenge.py
@@ -1,18 +1,22 @@
-challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+
+def simpsons():
+    challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
 
 
-trial= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
+    trial= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
 
 
-nightmare= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}},"banana": 15, "d": "nothing"}]
+    nightmare= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}},"banana": 15, "d": "nothing"}]
 
-#challenge print
+    #challenge print
 
-print("My", challenge[2][1] + "! The", challenge[2][0], "do", challenge[3]+"!")
+    print("My", challenge[2][1] + "! The", challenge[2][0], "do", challenge[3]+"!")
 
-#trial print
+    #trial print
 
-print("My", trial[2]["goggles"]+"! The", trial[2]["eyes"], "do nothing!")
+    print("My", trial[2]["goggles"]+"! The", trial[2]["eyes"], "do nothing!")
 
-#nightmare print
-print("My", nightmare[0]["user"]["name"]["first"] + "!", "The" , nightmare[0]["kumquat"], "do", nightmare[0]["d"] + "!" )
+    #nightmare print
+    print("My", nightmare[0]["user"]["name"]["first"] + "!", "The" , nightmare[0]["kumquat"], "do", nightmare[0]["d"] + "!" )
+
+simpsons()
